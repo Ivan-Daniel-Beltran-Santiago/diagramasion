437f997e8d01dc3388d51c97ac2cadb9a363507c
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2022-08-25 09:55:05 -0700

    Eliminado props, cambio en el routeamiento usado

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App isLoggedVisible = {true}/>
+    <App />
   </React.StrictMode>
 );
 

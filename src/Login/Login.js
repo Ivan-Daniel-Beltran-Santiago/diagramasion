6e66a19628ed526ebd3f64c166c9aed49a86101d
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2022-08-26 22:40:05 -0700

    Pretty-ficado

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import logo from '../Cabeza_Logo.jpg';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../Cabeza_Logo.jpg";
 
 export default function Login() {
-
   const [loginData, setLoginData] = useState({
-    id_number: '',
-    password: ''
+    id_number: "",
+    password: "",
   });
 
   const navigate = useNavigate();
 
   const handleInputChange = (event) => {
-    setLoginData({ ...loginData, [event.target.name]: event.target.value })
+    setLoginData({ ...loginData, [event.target.name]: event.target.value });
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    tryFetch();
+    //navigate("/Dashboard");
   }
 
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-    navigate('/Dashboard');
+  async function tryFetch() {
+    try {
+      const responde = await fetch('localhost/Login.php');
+      if (Response.ok) {
+        console.log('Piola');
+      } else {
+        console.log('Piolant');
+      }
+    } catch (error) {
+      console.log('Chale : ', error.message);
+    }
   }
 
   return (
@@ -27,13 +40,25 @@ export default function Login() {
         <h2 className="title">Iniciar sesión</h2>
         <form onSubmit={handleSubmit}>
           <h4>Matrícula/Número de control</h4>
-          <input type="text" name='id_number' placeholder="Matrícula/Número de control" onChange={handleInputChange} />
+          <input
+            type="text"
+            name="id_number"
+            placeholder="Matrícula/Número de control"
+            onChange={handleInputChange}
+          />
           <h4>Contraseña</h4>
-          <input type="password" name='password' placeholder="Contraseña" onChange={handleInputChange} />
+          <input
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            onChange={handleInputChange}
+          />
           <input type="submit" id="save" value="Ingresar" className="ingress" />
-          <p>Si es tu primera vez en el sistema, la contraseña es tu matrícula</p>
+          <p>
+            Si es tu primera vez en el sistema, la contraseña es tu matrícula
+          </p>
         </form>
       </div>
     </>
-  )
+  );
 }

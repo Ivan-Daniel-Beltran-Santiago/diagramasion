5bb0392570093c3664e0b57844c5132f1f9588b5
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2022-08-26 22:46:33 -0700

    Eliminados elementos de testeo de ruta

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./Dashboard/Dashboard";
 import Login from "./Login/Login";
-import Preferences from "./Preferences/Preferences";
 
 function App() {
   return (

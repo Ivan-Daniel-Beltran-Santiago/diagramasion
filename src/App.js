6a42c7ea5b90836ef5f623a517c2272cec3a8529
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2022-09-18 18:58:34 -0700

    Añadida nueva y posiblemente ultima ruta

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login/Login";
 import MenuEncargada from "./Menus/menuEncargada";
+import MenuEstudiante from "./Menus/menuEstudiante";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Menu-Encargada" element={<MenuEncargada/>}/>
+        <Route path="/Menu-Encargada" element={<MenuEncargada />} />
+        <Route path="/Menu-Estudiante" element={<MenuEstudiante />} />
       </Routes>
     </BrowserRouter>
   );

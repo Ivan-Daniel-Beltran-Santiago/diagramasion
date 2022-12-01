710c00d74faf6ddd4f35b05ac69ddbf9eb2d0f0a
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2022-12-01 00:34:36 -0700

    Casi posible cambiar contraseña y/o correo

--- src/Menus/Submodulos/informacionUsuario.js
+++ src/Menus/Submodulos/informacionUsuario.js
@@ -1,31 +1,155 @@
+import { useRef } from "react";
+import { Toast } from "primereact/toast";
+
 function InformacionUsuario({ currentUser }) {
+  const NewPassOne = useRef(null);
+  const NewPassTwo = useRef(null);
+  const NewEmail = useRef(null);
+  const CurrentPass = useRef(null);
+  const toast = useRef(null);
+
+  const showToast = (severityValue, summaryValue, detailValue) => {
+    toast.current.show({
+      closable: false,
+      life: 5000,
+      severity: severityValue,
+      summary: summaryValue,
+      detail: detailValue,
+    });
+  };
+
+  const updateChanges = () => {
+    let validChanges = false;
+    if (NewPassOne.current.value !== "" || NewEmail.current.value !== "") {
+      if (NewPassOne.current.value !== "") {
+        if (NewPassTwo.current.value === NewPassOne.current.value) {
+          const passwordRGEX = new RegExp("[0-9]{4,8}");
+          const newPassValid = passwordRGEX.test(NewPassOne.current.value);
+          if (newPassValid) {
+            validChanges = true;
+          } else {
+            showToast(
+              "warn",
+              "Cambio de contraseña invalido",
+              "La nueva contraseña no tiene un formato valido"
+            );
+            validChanges = false;
+          }
+        } else {
+          showToast(
+            "warn",
+            "Cambio de contraseña invalido",
+            "Ambas contraseñas deben coincidir"
+          );
+          validChanges = false;
+        }
+      }
+      if (NewEmail.current.value !== "") {
+        const emailRGEX = new RegExp(
+          "[A-Za-z0-9_.]{4,20}@[A-Za-z0-9_.]{4,10}.[A-Za-z0-9_.]{2,3}"
+        );
+        const newEmailValid = emailRGEX.test(NewEmail.current.value);
+        if (newEmailValid) {
+          validChanges = true;
+        } else {
+          showToast(
+            "warn",
+            "Cambio de correo invalido",
+            "El nuevo correo no tiene un formato valido"
+          );
+          validChanges = false;
+        }
+      }
+    } else {
+      showToast(
+        "warn",
+        "Operación invalida",
+        "No hay valores nuevos para cambiar"
+      );
+      validChanges = false;
+    }
+    if (validChanges) {
+      if (CurrentPass.current.value !== "") {
+        //Llamar a la api aqui
+      } else {
+        showToast(
+          "error",
+          "Operación invalida",
+          "Para realizar cambios debes colocar tu contraseña actual"
+        );
+      }
+    }
+  };
+
   return (
     <div id="informacionUsuario" className="modules">
+      <Toast ref={toast} position="top-right" />
       <form className="info_usuario">
         <label>Matricula:</label>
         <p>{currentUser.controlNumber}</p>
         <label>Nombre: </label>
         <p>{currentUser.fullName}</p>
+        {currentUser.currentCarrer !== undefined && (
+          <div>
+            <label>Carrera: </label>
+            <p>{currentUser.currentCarrer}</p>
+          </div>
+        )}
+        {currentUser.currentSemester !== undefined && (
+          <div>
+            <label>Semestre: </label>
+            <p>{currentUser.currentSemester}</p>
+          </div>
+        )}
       </form>
       <form className="info_usuario">
         <p>
           <label>Nueva contraseña: </label>
-          <input type="password" placeholder="Nueva contraseña"></input>
+          <input
+            ref={NewPassOne}
+            type="password"
+            placeholder="Nueva contraseña"
+            autoComplete="off"
+            maxLength={8}
+            minLength={4}
+          ></input>
         </p>
         <p>
           <label>Repetir nueva contraseña: </label>
-          <input type="password" placeholder="Nueva contraseña"></input>
+          <input
+            ref={NewPassTwo}
+            type="password"
+            placeholder="Nueva contraseña"
+            autoComplete="off"
+            maxLength={8}
+            minLength={4}
+          ></input>
         </p>
+        <h9>Debe contener entre 4 y 8 digitos </h9>
         <p>
           <label>Contraseña actual: </label>
-          <input type="password" placeholder="Contraseña actual"></input>
+          <input
+            ref={CurrentPass}
+            type="password"
+            placeholder="Contraseña actual"
+            autoComplete="off"
+            maxLength={8}
+            minLength={4}
+          ></input>
         </p>
         <p>
           <label>Correo electrónico: </label>
-          <input type="email" placeholder={currentUser.eMail}></input>
+          <input
+            ref={NewEmail}
+            type="email"
+            placeholder={currentUser.eMail}
+            maxLength={34}
+          ></input>
         </p>
       </form>
-      <button className="confirmarCambios">Confirmar cambios</button>
+      <button className="confirmarCambios" onClick={updateChanges}>
+        Confirmar cambios
+      </button>
     </div>
   );
 }

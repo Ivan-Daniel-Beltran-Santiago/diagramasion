4fdd5085dcda2f019f0574fea838bc6537757e06
Ivan-Daniel-Beltran-Santiago <starfoxfan950@hotmail.com>
2022-12-19 11:02:47 -0700

    Ya sube Estudiantes desde el excel wn

--- src/Menus/Submodulos/administracionGeneral.js
+++ src/Menus/Submodulos/administracionGeneral.js
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import axios from "axios";
+import { useRef, useState } from "react";
+import ServerConnectionConfig from "../../Controller/ServerConnectionConfig";
+import * as XLSX from "xlsx";
+import { Toast } from "primereact/toast";
 
 function AdministracionGeneral() {
   const [dateRanges, setDateRanges] = useState({
@@ -6,6 +10,19 @@ function AdministracionGeneral() {
     upperRange: "",
   });
   const [validDataRange, setValidDataRange] = useState(false);
+  const [newUsers, setNewUsers] = useState([]);
+
+  const toast = useRef(null);
+
+  const showToast = (severityValue, summaryValue, detailValue) => {
+    toast.current.show({
+      closable: false,
+      life: 5000,
+      severity: severityValue,
+      summary: summaryValue,
+      detail: detailValue,
+    });
+  };
 
   const validateInputChange = (event) => {
     const lower = dateRanges.lowerRange ?? "none";
@@ -29,8 +46,85 @@ function AdministracionGeneral() {
     validateInputChange(event);
   };
 
+  const handleNewUsers = (array) => {
+    const newUserArray = [...newUsers];
+    // eslint-disable-next-line array-callback-return
+    array.some((file) => {
+      if (newUserArray.findIndex((f) => (f[0] === file[0]) === -1)) {
+        newUserArray.push(file);
+      }
+    });
+    setNewUsers(newUserArray);
+    showToast(
+      "success",
+      "Registros de Usuarios",
+      "Usuarios listos para cargarse a la base de datos"
+    );
+  };
+
+  const handleFileChange = async (event) => {
+    event.preventDefault();
+    setNewUsers([{}]);
+    const file = event.target.files[0];
+    const data = await file.arrayBuffer();
+    const workbook = XLSX.read(data);
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+      header: 1,
+      defval: "",
+    });
+    handleNewUsers(jsonData);
+  };
+
+  const uploadNewUsers = (event) => {
+    const srvDir = new ServerConnectionConfig();
+    const srvReq = srvDir.getServer() + "/SubirUsuarios";
+
+    for (var indice = 0; indice < newUsers.length; indice++) {
+      let matricula = newUsers[indice][0];
+      let nombre_Completo = newUsers[indice][1];
+      let contraseña = newUsers[indice][0];
+      let correo_e = newUsers[indice][2];
+      let carrera = newUsers[indice][3];
+      let semestre = newUsers[indice][4];
+      if (matricula === "Matricula") {
+        continue;
+      }
+      axios
+        .post(srvReq, {
+          matriculaUser: matricula,
+          nombreUser: nombre_Completo,
+          contraseñaUser: contraseña,
+          correoUser: correo_e,
+          carreraUser: carrera,
+          semestreUser: semestre,
+        })
+        .then((result) => {
+          console.log(result.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  const obtenerEstadisticas = (event) => {
+    event.preventDefault();
+    const srvDir = new ServerConnectionConfig();
+    const srvReq = srvDir.getServer() + "/ObtenerConteoEstadistico";
+    axios
+      .get(srvReq, { params: dateRanges })
+      .then((result) => {
+        console.log(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div id="administracionGeneral" className="modules">
+      <Toast ref={toast} position="top-right" />
       <div className="dateContainer">
         <p>
           <label className="labelDate">Fecha de inicio</label>
@@ -58,7 +152,9 @@ function AdministracionGeneral() {
             La fecha de finalización no puede ser menor a la fecha de inicio.
           </label>
         )}
-        <button>Generar Informe Estadístico</button>
+        <button onClick={obtenerEstadisticas}>
+          Generar Informe Estadístico
+        </button>
       </div>
       <div>
         <br />
@@ -69,12 +165,15 @@ function AdministracionGeneral() {
             type="file"
             id="subirArchivos"
             name="Subir archivo Excel"
+            accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            onChange={handleFileChange}
           ></input>
           <p>
             <input
-              type="submit"
+              type="button"
               className="loadLogin"
               value="Cargar Inicios de Sesión"
+              onClick={uploadNewUsers}
             ></input>
           </p>
         </form>

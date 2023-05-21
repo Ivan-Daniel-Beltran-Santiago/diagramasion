bf7fc511e73f594908d6543a5fc0fb01aa391101
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2023-05-21 14:04:02 -0700

    Menu de usuario universal

---
+++ src/Vista/MenuUsuario.js
@@ -0,0 +1,144 @@
+import React from "react";
+import { Toast } from "primereact/toast";
+
+import "./MenuUsuario.css";
+import LogoHeader from "../View/Auxiliary/Logo_Header";
+import MenuControlador from "../Controlador/MenuControlador";
+import RegresarMenu from "../View/Auxiliary/regresarMenu";
+
+function MenuUsuario({ MenuUsuarioTostado, MenuIndex, MenuCambiarIndex, MenuInformacionUsuario, MenuUsuarioSetUsuarioActivo }) {
+  return (
+    <div className="App">
+      <LogoHeader />
+      <div>
+        <div>
+        <Toast ref={MenuUsuarioTostado} position="top-right" />
+          <div className="content-section">
+            <div className="contentSelector">
+              {MenuInformacionUsuario.Estudiante !== null && (
+                <div>
+                  <button
+                    id="bienvenidaEstudiante"
+                    className="button"
+                    onClick={() => MenuCambiarIndex(1)}
+                  >
+                    Bienvenida
+                  </button>
+                  <button
+                    id="administrarSolicitudes"
+                    className="botonTramite"
+                    onClick={() => MenuCambiarIndex(2)}
+                  >
+                    Solicitar Tramite
+                  </button>
+                  <button
+                    id="solicitudEstudiante"
+                    className="button"
+                    onClick={() => MenuCambiarIndex(3)}
+                  >
+                    Mis Solicitudes
+                  </button>
+                  <button
+                    id="informacionUsuario"
+                    className="button"
+                    onClick={() => MenuCambiarIndex(4)}
+                  >
+                    Mi cuenta
+                  </button>
+                </div>
+              )}
+              {MenuInformacionUsuario.Estudiante === null && (
+                <div>
+                  <button
+                    id="bienvenidaEncargada"
+                    className="button"
+                    onClick={() => {
+                      MenuCambiarIndex(5);
+                    }}
+                  >
+                    Bienvenida
+                  </button>
+                  <button
+                    id="administrarSolicitudes"
+                    className="button"
+                    onClick={() => {
+                      MenuCambiarIndex(6);
+                    }}
+                  >
+                    Administración de Solicitudes
+                  </button>
+                  <button
+                    id="administrarSolicitud"
+                    className="button"
+                    onClick={() => {
+                      MenuCambiarIndex(7);
+                    }}
+                  >
+                    Administrar Solicitud Individual
+                  </button>
+                  <button
+                    id="administracionGeneral"
+                    className="button"
+                    onClick={() => {
+                      MenuCambiarIndex(8);
+                    }}
+                  >
+                    Informe Estadistico
+                  </button>
+                  <button
+                    id="administracionUsuarios"
+                    className="button"
+                    onClick={() => {
+                      MenuCambiarIndex(9);
+                    }}
+                  >
+                    Administración de Usuarios
+                  </button>
+                  <button
+                    id="administracionTramites"
+                    className="button"
+                    onClick={() => {
+                      MenuCambiarIndex(10);
+                    }}
+                  >
+                    Administración de Tramites
+                  </button>
+                  <button
+                    id="edicionCorreos"
+                    className="button"
+                    onClick={() => {
+                      MenuCambiarIndex(11);
+                    }}
+                  >
+                    Administración de Correos
+                  </button>
+                  <button
+                    id="informacionUsuario"
+                    className="button"
+                    onClick={() => {
+                      MenuCambiarIndex(12);
+                    }}
+                  >
+                    Mi cuenta
+                  </button>
+                </div>
+              )}
+            </div>
+            <div>
+              <div className="content">
+                <MenuControlador
+                  submenuIndex={MenuIndex}
+                  submenuUsuario={MenuInformacionUsuario}
+                  MenuControladorSetUsuarioActivo={MenuUsuarioSetUsuarioActivo}
+                />
+                <RegresarMenu />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MenuUsuario;

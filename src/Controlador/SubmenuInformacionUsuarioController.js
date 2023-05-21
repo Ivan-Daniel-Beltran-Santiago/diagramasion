31f35aaa5f815b7414865df9c7d0cb45f6d8d02c
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2023-05-21 14:02:37 -0700

    Terminada funcionalidad de "Mi Cuenta"

---
+++ src/Controlador/SubmenuInformacionUsuarioController.js
@@ -0,0 +1,159 @@
+import { useRef, useState } from "react";
+import axios from "axios";
+import SubmenuInformacionUsuario from "../Vista/SubmenuInformacionUsuario";
+import ConfigurarConexion from "./ConfigurarConexion";
+
+const SubmenuInformacionUsuarioControlador = ({
+  SubmenuInformacionUsuarioControladorUsuarioActual,
+  SubmenuInformacionUsuarioControladorSetUsuarioActual,
+}) => {
+  //Variables de estado.
+  const [informacionUsuarioActualizar, setInformacionUsuarioActualizar] =
+    useState({
+      matricula: SubmenuInformacionUsuarioControladorUsuarioActual.matricula,
+      nuevoCorreo_e: "",
+      nuevaContraseña: "",
+      nuevaContraseñaConfirmar: "",
+      contraseñaActual: "",
+    });
+
+  const [validacionInformacionUsuario, setValidacionInformacionUsuario] =
+    useState({
+      nuevoCorreo_e: false,
+      nuevaContraseña: false,
+      nuevaContraseñaConfirmar: false,
+      nuevaContraseñaCoincide: false,
+      contraseñaActual: false,
+    });
+
+  //Alertas.
+  const toast = useRef(null);
+
+  const showToast = (severityValue, summaryValue, detailValue) => {
+    toast.current.show({
+      closable: false,
+      life: 5000,
+      severity: severityValue,
+      summary: summaryValue,
+      detail: detailValue,
+    });
+  };
+
+  //Detectar los cambios en los campos de texto.
+  const handleInputChange = (event) => {
+    setInformacionUsuarioActualizar({
+      ...informacionUsuarioActualizar,
+      [event.target.name]: event.target.value,
+    });
+    validateInputChange(event);
+  };
+
+  //Valida los cambios en los campos de texto.
+  const validateInputChange = (event) => {
+    //Validaciones.
+    var validarContraseña = new RegExp("^[0-9]{3,8}");
+    var validarCorreoElectronico = new RegExp(
+      "^(?!$)[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*\\.[A-Za-z]{2,}(.[A-Za-z]{2,})?$"
+    );
+
+    switch (event.target.name) {
+      case "nuevoCorreo_e":
+        setValidacionInformacionUsuario({
+          ...validacionInformacionUsuario,
+          [event.target.name]: validarCorreoElectronico.test(
+            event.target.value
+          ),
+        });
+        break;
+      case "nuevaContraseña":
+      case "nuevaContraseñaConfirmar":
+        setValidacionInformacionUsuario({
+          ...validacionInformacionUsuario,
+          [event.target.name]: validarContraseña.test(event.target.value),
+        });
+        setValidacionInformacionUsuario({
+          ...validacionInformacionUsuario,
+          nuevaContraseñaCoincide:
+            validacionInformacionUsuario.nuevaContraseña ===
+            validacionInformacionUsuario.nuevaContraseñaConfirmar,
+        });
+        break;
+      default:
+        setValidacionInformacionUsuario({
+          ...validacionInformacionUsuario,
+          [event.target.name]: validarContraseña.test(event.target.value),
+        });
+        break;
+    }
+  };
+
+  //Actualiza los campos deseados.
+  const actualizarDatosUsuario = async () => {
+    const servidor = new ConfigurarConexion();
+    const funcion = servidor.obtenerServidor() + "/usuarios/actualizar";
+
+    const actualizacion = await axios.put(funcion, {
+      matricula: informacionUsuarioActualizar.matricula,
+      contraseña: informacionUsuarioActualizar.contraseñaActual,
+      nuevaContraseña: informacionUsuarioActualizar.nuevaContraseña,
+      correo_e: informacionUsuarioActualizar.nuevoCorreo_e,
+    });
+
+    switch (actualizacion.status) {
+      case 200:
+        SubmenuInformacionUsuarioControladorSetUsuarioActual(
+          actualizacion.data
+        );
+        setTimeout(() => {
+          showToast(
+            "success",
+            "Usuario",
+            "Correo electronico y/o contraseña actualizados exitosamente."
+          );
+        }, 100);
+        break;
+      case 400:
+        setTimeout(() => {
+          showToast(
+            "error",
+            "Usuario",
+            "Formato de correo electronico y/o contraseña invalidos."
+          );
+        }, 100);
+        break;
+      case 404:
+        setTimeout(() => {
+          showToast(
+            "error",
+            "Usuario",
+            "Usuario no encontrado, contacte al administrador."
+          );
+        }, 100);
+        break;
+      default:
+        setTimeout(() => {
+          showToast(
+            "error",
+            "Usuario",
+            "Error del servidor, contacte al administrador."
+          );
+        }, 100);
+        break;
+    }
+  };
+
+  return (
+    <SubmenuInformacionUsuario
+      InfoUsuarioTostado={toast}
+      HandleInputChange={handleInputChange}
+      ActualizarDatosUsuario={actualizarDatosUsuario}
+      InformacionUsuarioActualizar={informacionUsuarioActualizar}
+      ValidacionInformacionUsuario={validacionInformacionUsuario}
+      SubmenuInformacionUsuarioUsuarioActual={
+        SubmenuInformacionUsuarioControladorUsuarioActual
+      }
+    />
+  );
+};
+
+export default SubmenuInformacionUsuarioControlador;
---
+++ src/Vista/SubmenuInformacionUsuario.js
@@ -0,0 +1,162 @@
+import React from "react";
+import { Toast } from "primereact/toast";
+
+import styled from "styled-components";
+const Container = styled.div`
+  padding: 1em;
+`;
+
+function SubmenuInformacionUsuario({
+  InfoUsuarioTostado,
+  HandleInputChange,
+  SubmenuInformacionUsuarioUsuarioActual,
+  InformacionUsuarioActualizar,
+  ValidacionInformacionUsuario,
+  ActualizarDatosUsuario,
+}) {
+  return (
+    <Container>
+      <div id="informacionUsuario" className="modules">
+        <Toast ref={InfoUsuarioTostado} position="top-right" />
+        <form className="info_usuario">
+          <label className="Indicador">Matricula:⠀ </label>
+          <label> {SubmenuInformacionUsuarioUsuarioActual.matricula}</label>
+          <br />
+          <br />
+          <label className="Indicador">Nombre:⠀ </label>
+          <label>
+            {" "}
+            {SubmenuInformacionUsuarioUsuarioActual.nombre_Completo}
+          </label>
+          <br />
+          <br />
+          {SubmenuInformacionUsuarioUsuarioActual.carrera !== undefined && (
+            <div>
+              <label className="Indicador">Carrera:⠀ </label>
+              <label> {SubmenuInformacionUsuarioUsuarioActual.carrera}</label>
+            </div>
+          )}
+          {SubmenuInformacionUsuarioUsuarioActual.semestre !== undefined && (
+            <div>
+              <label className="Indicador">Semestre:⠀ </label>
+              <label> {SubmenuInformacionUsuarioUsuarioActual.semestre}</label>
+            </div>
+          )}
+        </form>
+        <form className="info_usuario">
+          <br />
+          <label className="Indicador">
+            Para cambiar su contraseña. (Opcional)
+          </label>
+          <p className="modules">
+            <p>
+              <label>Nueva contraseña: </label>
+              <input
+                type="password"
+                placeholder="Nueva contraseña"
+                autoComplete="off"
+                name="nuevaContraseña"
+                id="newPassword"
+                onChange={HandleInputChange}
+              ></input>
+            </p>
+            <p>
+              <label>Repetir nueva contraseña: </label>
+              <input
+                type="password"
+                placeholder="Repetir nueva contraseña"
+                autoComplete="off"
+                name="nuevaContraseñaConfirmar"
+                id="confirmPassword"
+                onChange={HandleInputChange}
+              ></input>
+            </p>
+            {((!ValidacionInformacionUsuario.nuevaContraseña &&
+              InformacionUsuarioActualizar.nuevaContraseña.length > 0) ||
+              (!ValidacionInformacionUsuario.nuevaContraseñaConfirmar &&
+                InformacionUsuarioActualizar.nuevaContraseñaConfirmar.length >
+                  0)) && (
+              <label className="LoginWarning">
+                La nueva contraseña debe contener entre 4 y 8 digitos{" "}
+              </label>
+            )}
+            <br />
+            {!ValidacionInformacionUsuario.nuevaContraseñaCoincide &&
+              InformacionUsuarioActualizar.nuevaContraseña.length > 0 &&
+              InformacionUsuarioActualizar.nuevaContraseñaConfirmar.length >
+                0 && (
+                <label className="LoginWarning">
+                  La contraseña debe coindidir en ambos campos
+                </label>
+              )}
+          </p>
+          <label className="Indicador">
+            Para cambiar su correo electrónico: (Opcional)
+          </label>
+          <p className="modules">
+            <p>
+              <label>Correo Electronico: </label>
+              <input
+                type="email"
+                placeholder={SubmenuInformacionUsuarioUsuarioActual.correo_e}
+                name="nuevoCorreo_e"
+                id="newEmail"
+                onChange={HandleInputChange}
+              ></input>
+            </p>
+            {!ValidacionInformacionUsuario.nuevoCorreo_e &&
+              InformacionUsuarioActualizar.nuevoCorreo_e.length > 0 && (
+                <div>
+                  <label className="LoginWarning">
+                    El correo electronico debe seguir el siguiente formato:
+                  </label>
+                  <br />
+                  <label className="LoginWarning">
+                    -Al menos un caracter/digito antes y despues del @
+                  </label>
+                  <br />
+                  <label className="LoginWarning">
+                    -Se permiten solo los simbolos @ y .{" "}
+                  </label>
+                  <br />
+                  <label className="LoginWarning">
+                    -De 2 a 3 caracteres al final como el .com o .mx o similares
+                  </label>
+                </div>
+              )}
+          </p>
+          {(ValidacionInformacionUsuario.nuevoCorreo_e ||
+            (ValidacionInformacionUsuario.nuevaContraseña &&
+              ValidacionInformacionUsuario.nuevaContraseñaCoincide)) && (
+            <div>
+              <label className="Indicador">
+                Para actualizar los datos, ingrese su contraseña actual:{" "}
+              </label>
+              <p className="modules">
+                <label className="Indicador">Contraseña actual:</label>{" "}
+                <label className="Obligatorio">*</label>
+                <input
+                  type="password"
+                  placeholder="Contraseña actual"
+                  autoComplete="off"
+                  name="contraseñaActual"
+                  id="contraseñaUsuario"
+                  onChange={HandleInputChange}
+                ></input>
+              </p>
+            </div>
+          )}
+        </form>
+        {(ValidacionInformacionUsuario.nuevoCorreo_e ||
+          (ValidacionInformacionUsuario.nuevaContraseña &&
+            ValidacionInformacionUsuario.nuevaContraseñaCoincide)) && (
+          <button className="confirmarCambios" onClick={ActualizarDatosUsuario}>
+            Confirmar cambios
+          </button>
+        )}
+      </div>
+    </Container>
+  );
+}
+
+export default SubmenuInformacionUsuario;
